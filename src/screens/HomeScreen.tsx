90e6a1e0048c4c7e60f6d2fc799e43d06096fd80
k-1-e <obregonkevin1351@gmail.com>
2021-11-29 18:09:41 -0600

    agregando scrollview

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,117 +1,114 @@
-import { Button, Text, View, StyleSheet, TextInput, TouchableOpacity, } from 'react-native';
+import { Button, Text, View, StyleSheet, TextInput, TouchableOpacity, ScrollView, } from 'react-native';
 import React, { useState, useEffect } from 'react'
 // import React from 'react'; {useState, } from 'react'
 
 const FeedScreen = ({ navigation }) => {
 
-  const [tarea, setTarea] = useState<string>('')
-  const [tareas, setTareas] = useState<string[]>([])
+    const [tarea, setTarea] = useState<string>('')
+    const [tareas, setTareas] = useState<string[]>([])
 
-  // let tareas: string[] = []
 
-  // const [usuario , setUsuario] = useState('')
-  const crearTarea = () => {
-    setTareas([...tareas, tarea])
-  }
 
-  const eliminarTarea = (id: number) => {
-    tareas.splice(id, 1)
-    getTareas()
-  }
-  const getTareas = () => {
-    setTareas([...tareas])
-  }
+    // const [usuario , setUsuario] = useState('')
+    const crearTarea = () => {
+        setTareas([...tareas, tarea])
+    }
 
-  useEffect(getTareas, [])
+    const eliminarTarea = (id: number) => {
+        tareas.splice(id, 1)
+        getTareas()
+    }
+    const getTareas = () => {
+        setTareas([...tareas])
+    }
 
-  return (
-    <View style={styles.containerBase}>
-      <View style={styles.container}>
-        <TextInput
-          placeholder={"ingresar tarea"}
-          onChangeText={setTarea}
-        />
-        <Button
-          title="agregar"
-          onPress={crearTarea}
-        />
-      </View>
+    useEffect(getTareas, [])
 
-      {
-        tareas.map((lista, index) => (
-          <View style={styles.container} key={index}>
-            {/* <Text style={styles.text} key={lista.id}>{lista.name}</Text> */}
-            <Text style={styles.text}>{lista}</Text>
+    return (
+        <ScrollView>
+        <View style={styles.containerBase}>
+            <View style={styles.container}>
+                <TextInput
+                    placeholder={"ingresar tarea"}
+                    onChangeText={setTarea}
+                />
+                <Button
+                    title="agregar"
+                    onPress={crearTarea}
+                />
+            </View>
 
-            <Button
-              color="red"
-              title="Eliminar"
-              onPress={() => eliminarTarea(index)}
-            />
-            {/* <Button
-                    color="green"
-                    title="Editar"
-                    onPress={editarTarea}
-                /> */}
+            {
+                tareas.map((lista, index) => (
+                    <View style={styles.container} key={index}>
 
-          </View>
+                        <Text style={styles.text}>{lista}</Text>
 
-        ))
-      }
-     
-    
-    </View>
-  )
+                        <Button
+                            color="red"
+                            title="Eliminar"
+                            onPress={() => eliminarTarea(index)}
+                        />
+
+
+                    </View>
+
+                ))
+            }
+
+        </View>
+        </ScrollView>
+    )
 }
 export default FeedScreen
 
 const styles = StyleSheet.create({
 
-  container: {
-    width: '95%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: 'white',
-    marginBottom: 15,
-    paddingVertical: 8,
-    paddingHorizontal: 8,
-    marginLeft: 10,
-    borderWidth: 2,
-    borderRadius: 10
-  },
-  containerBase: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-  },
+    container: {
+        width: '95%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'white',
+        marginBottom: 15,
+        paddingVertical: 8,
+        paddingHorizontal: 8,
+        marginLeft: 10,
+        borderWidth: 2,
+        borderRadius: 10
+    },
+    containerBase: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+    },
 
-  textbotonInsertar: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 25,
-    borderRadius: 50,
-  },
-  text: {
-    fontSize: 34,
-    color: '#034C50',
-    width: '70%'
+    textbotonInsertar: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 25,
+        borderRadius: 50,
+    },
+    text: {
+        fontSize: 34,
+        color: '#034C50',
+        width: '70%'
 
-  },
+    },
 
-  button: {
-    color: '#C4C4C4',
-    marginTop: 50
-  },
+    button: {
+        color: '#C4C4C4',
+        marginTop: 50
+    },
 
-  btnagregar: {
-    backgroundColor: 'black',
-    marginTop: '70%',
-    borderRadius: 8,
-  },
-  base: {
-    flex: 1,
-    backgroundColor: '#f5e3e1'
+    btnagregar: {
+        backgroundColor: 'black',
+        marginTop: '70%',
+        borderRadius: 8,
+    },
+    base: {
+        flex: 1,
+        backgroundColor: '#f5e3e1'
 
-  },
+    },
 
 
 
